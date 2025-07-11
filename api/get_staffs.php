0a460da79d4a09d530d8be3767b4b11e6e69e925
Long KenZy <it.htlong25@gmail.com>
2025-07-11 09:17:56 +0700

    sua api

--- api/get_staffs.php
+++ api/get_staffs.php
@@ -36,11 +36,6 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     exit();
 }
 
-if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
-    http_response_code(403);
-    exit('Access denied.');
-}
-
 try {
     // ===== LẤY THAM SỐ TỪ URL ===== //
     
